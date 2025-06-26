a30743b1fe9787f21b9da92f6127d2703f2425ff
Stefan <git@stefn.co.uk>
2025-06-26 13:33:18 +0100

    Unify the safety standards bits

--- spec/controllers/pdf_filename_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-RSpec.describe "PDF Content Changes", type: :request do
-  describe "Serial Number label" do
-    it "shows 'Serial Number' instead of 'Serial Number / Asset ID'" do
-      expect(I18n.t("pdf.inspection.fields.serial")).to eq("Serial Number")
-    end
-  end
-end
