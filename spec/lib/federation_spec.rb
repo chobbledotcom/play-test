93c336fb0c528447b03d264399bf2323abae3476
Stefan <git@stefn.co.uk>
2025-07-24 23:34:53 +0100

    Search all sites when logged out too

--- spec/lib/federation_spec.rb
+++ spec/lib/federation_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "rails_helper"
 
 RSpec.describe Federation do
@@ -9,36 +11,20 @@ RSpec.describe Federation do
         sites = Federation.sites
         expect(sites.length).to eq(3)
         site_names = sites.map { |s| s[:name] }
-        expect(site_names).to eq([:current_site, :play_test, :rpii_play_test])
+        expect(site_names).to eq(%i[current_site play_test rpii_play_test])
       end
 
       it "excludes sites matching the current host" do
         sites = Federation.sites("play-test.co.uk")
         expect(sites.length).to eq(2)
         site_names = sites.map { |s| s[:name] }
-        expect(site_names).to eq([:current_site, :rpii_play_test])
+        expect(site_names).to eq(%i[current_site rpii_play_test])
       end
 
       it "excludes rpii site when host matches" do
         sites = Federation.sites("rpii.play-test.co.uk")
         expect(sites.length).to eq(2)
-        expect(sites.map { |s| s[:name] }).to eq([:current_site, :play_test])
-      end
-    end
-
-    context "in production environment" do
-      before { allow(Rails.env).to receive(:local?).and_return(false) }
-
-      it "returns only current_site" do
-        sites = Federation.sites
-        expect(sites.length).to eq(1)
-        expect(sites.first[:name]).to eq(:current_site)
-      end
-
-      it "still returns only current_site when host is provided" do
-        sites = Federation.sites("play-test.co.uk")
-        expect(sites.length).to eq(1)
-        expect(sites.first[:name]).to eq(:current_site)
+        expect(sites.map { |s| s[:name] }).to eq(%i[current_site play_test])
       end
     end
   end
