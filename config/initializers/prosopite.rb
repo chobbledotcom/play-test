f2b7f8ed9891175a58ad5e45e29c90243bbf804f
stefan burke <83819576+stefan-burke@users.noreply.github.com>
2025-08-04 02:58:57 +0000

    Run everywhere

--- config/initializers/prosopite.rb
+++ config/initializers/prosopite.rb
@@ -1,33 +1,29 @@
 # frozen_string_literal: true
 
-unless Rails.env.production?
-  require "prosopite"
+require "prosopite"
 
-  Rails.application.config.after_initialize do
-    Prosopite.rails_logger = Rails.logger
-    Prosopite.prosopite_logger = true
+Rails.application.config.after_initialize do
+  Prosopite.rails_logger = Rails.logger
+  Prosopite.prosopite_logger = true
 
-    Prosopite.allow_stack_paths = [
-      "active_storage",
-      "active_record/associations/preloader",
-      "mission_control",
-      "solid_queue"
-    ]
+  Prosopite.allow_stack_paths = [
+    "mission_control",
+    "solid_queue"
+  ]
 
-    Prosopite.custom_logger = if defined?(Sentry)
-      lambda do |message|
-        Rails.logger.warn(message)
-        Sentry.capture_message(
-          "N+1 Query Detected: #{message}",
-          level: :warning,
-          extra: {
-            prosopite_message: message,
-            backtrace: caller(5, 10)
-          }
-        )
-      end
-    else
-      Rails.logger
+  Prosopite.custom_logger = if defined?(Sentry)
+    lambda do |message|
+      Rails.logger.warn(message)
+      Sentry.capture_message(
+        "N+1 Query Detected: #{message}",
+        level: :warning,
+        extra: {
+          prosopite_message: message,
+          backtrace: caller(5, 10)
+        }
+      )
     end
+  else
+    Rails.logger
   end
 end
