523e6d998fde4e0df77a39be9be60d824ef3cbe1
stefan burke <83819576+stefan-burke@users.noreply.github.com>
2025-08-04 04:15:07 +0100

    fix prosopite (#87)
    * Fix prosopite (hopefully)
    
    * Fix prosopite tests

--- spec/lib/prosopite_integration_spec.rb
+++ spec/lib/prosopite_integration_spec.rb
@@ -4,12 +4,14 @@ require "rails_helper"
 
 RSpec.describe "Prosopite N+1 detection" do
   it "is configured in ApplicationController" do
-    expect(ApplicationController.private_instance_methods).to include(:n_plus_one_detection)
+    methods = ApplicationController.private_instance_methods
+    expect(methods).to include(:n_plus_one_detection)
   end
 
-  it "is disabled in test environment" do
+  it "is enabled in test environment" do
     controller = ApplicationController.new
-    expect(controller._process_action_callbacks.map(&:filter)).not_to include(:n_plus_one_detection)
+    callbacks = controller._process_action_callbacks.map(&:filter)
+    expect(callbacks).to include(:n_plus_one_detection)
   end
 
   it "would be enabled in development environment" do
@@ -17,7 +19,7 @@ RSpec.describe "Prosopite N+1 detection" do
     allow(Rails.env).to receive(:test?).and_return(false)
     allow(Rails.env).to receive(:production?).and_return(false)
 
-    # This would require reloading the controller, so we just verify the condition
+    # This would require reloading the controller, so we just verify condition
     expect(Rails.env.production? || Rails.env.test?).to be false
   end
 end
