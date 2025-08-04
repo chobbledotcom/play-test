f0eda60ae2f71a727a3629dc9f025ea63d3c27b7
stefan burke <83819576+stefan-burke@users.noreply.github.com>
2025-08-04 22:41:19 +0100

    refactor(sorbet): add Sorbet type signatures and strictness to units-related code (#115)

--- app/models/concerns/custom_id_generator.rb
+++ app/models/concerns/custom_id_generator.rb
@@ -1,5 +1,9 @@
+# typed: true
+# frozen_string_literal: true
+
 module CustomIdGenerator
   extend ActiveSupport::Concern
+  extend T::Sig
 
   # Standard ID length for all models using CustomIdGenerator
   ID_LENGTH = 8
@@ -10,6 +14,9 @@ module CustomIdGenerator
   end
 
   class_methods do
+    extend T::Sig
+
+    sig { params(scope_conditions: T::Hash[T.untyped, T.untyped]).returns(String) }
     def generate_random_id(scope_conditions = {})
       loop do
         id = SecureRandom.alphanumeric(CustomIdGenerator::ID_LENGTH).upcase
@@ -20,6 +27,7 @@ module CustomIdGenerator
 
   private
 
+  sig { void }
   def generate_custom_id
     scope_conditions = respond_to?(:uniqueness_scope) ? uniqueness_scope : {}
     self.id = self.class.generate_random_id(scope_conditions)
