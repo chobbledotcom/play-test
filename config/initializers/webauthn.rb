5ab0f085c8210652be1db967a3ca9532bb2005c6
Stefan <git@stefn.co.uk>
2025-08-05 18:25:47 +0100

    Simpler base_url and app_name stuff

--- config/initializers/webauthn.rb
+++ config/initializers/webauthn.rb
@@ -1,11 +1,11 @@
 WebAuthn.configure do |config|
   # Use BASE_URL from environment
-  base_url = ENV["BASE_URL"] || "http://localhost:3000"
+  base_url = ENV["BASE_URL"]
 
   config.allowed_origins = [base_url]
 
   # Use APP_NAME from environment
-  config.rp_name = ENV["APP_NAME"] || "PlayTest2"
+  config.rp_name = ENV["APP_NAME"]
 
   # Extract domain from BASE_URL
   config.rp_id = URI.parse(base_url).host
