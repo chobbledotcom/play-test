dde3ec9b8cb4a82a735c5bea1802954ee9c7a9b2
Stefan <git@stefn.co.uk>
2025-08-06 03:07:04 +0100

    Standardrb-ize

--- spec/features/errors_spec.rb
+++ spec/features/errors_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.feature "Error pages", type: :feature do
   scenario "404 page uses application layout" do
     visit "/non-existent-page-that-should-not-exist"
-    
+
     expect(page.status_code).to eq(404)
     expect(page).to have_content(I18n.t("errors.not_found.title"))
     expect(page).to have_content(I18n.t("errors.not_found.message"))
