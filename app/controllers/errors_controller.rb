8baf371bdc36ef5455fd4c51019bdca495874078
Stefan <git@stefn.co.uk>
2025-08-07 09:16:30 +0100

    Don't show the 404/500 error pages for missing CSS etc files, only html

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -6,10 +6,24 @@ class ErrorsController < ApplicationController
   skip_before_action :update_last_active_at
 
   def not_found
-    render status: :not_found
+    respond_to do |format|
+      format.html { render status: :not_found }
+      format.json do
+        render json: {error: I18n.t("errors.not_found.title")},
+          status: :not_found
+      end
+      format.any { head :not_found }
+    end
   end
 
   def internal_server_error
-    render status: :internal_server_error
+    respond_to do |format|
+      format.html { render status: :internal_server_error }
+      format.json do
+        render json: {error: I18n.t("errors.internal_server_error.title")},
+          status: :internal_server_error
+      end
+      format.any { head :internal_server_error }
+    end
   end
 end
