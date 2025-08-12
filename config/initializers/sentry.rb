be681e3e48d54c3701ff4edd13ed133dbf283bcb
Stefan <git@stefn.co.uk>
2025-08-12 15:59:32 +0100

    Don't filter any Sentry errors

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -13,23 +13,6 @@ Sentry.init do |config|
   config.send_default_pii = false
 
   config.before_send = lambda do |event, hint|
-    if hint[:exception]
-      exception = hint[:exception]
-
-      # Use the same filtering logic as ntfy notifications
-      if event.contexts && event.contexts[:rack_env]
-        env = event.contexts[:rack_env]
-        if env["action_controller.instance"]
-          controller = env["action_controller.instance"]
-
-          # Use ApplicationController's should_notify_error? method
-          unless controller.send(:should_notify_error?, exception)
-            return nil
-          end
-        end
-      end
-    end
-
     if defined?(Current) && Current.user
       event.user = {
         id: Current.user.id,
