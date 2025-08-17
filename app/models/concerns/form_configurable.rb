149bf22d110f54a8be1a57c84e971a719e02f1e8
stefan burke <83819576+stefan-burke@users.noreply.github.com>
2025-08-17 13:16:20 +0000

    Update form_configurable.rb

--- app/models/concerns/form_configurable.rb
+++ app/models/concerns/form_configurable.rb
@@ -19,7 +19,7 @@ module FormConfigurable
       file_name = name.demodulize.underscore
       config_path = Rails.root.join("config/forms/#{file_name}.yml")
       yaml_content = YAML.load_file(config_path)
-      yaml_content.deep_symbolize_keys
+      yaml_content.deep_symbolize_keys!
       yaml_content[:form_fields]
     end
   end
