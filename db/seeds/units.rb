f722d4c263fceccc302e9bb042eb57c0fe20b797
stefan burke <icjv0qtf@4wrd.cc>
2025-10-13 14:50:39 +0000

    feat(db/seeds): associate badges with units by creating badge batches
    - Created a badge batch and badges to link with units in seed data.
    - Modified create_unit method to accept and assign badge IDs.
    - Updated unit factory to support badge association via :with_badge trait.
    - Enhanced seed completeness spec to account for badge id system column.
    
    Co-authored-by: terragon-labs[bot] <terragon-labs[bot]@users.noreply.github.com>

--- db/seeds/units.rb
+++ db/seeds/units.rb
@@ -1,8 +1,14 @@
 Rails.logger.debug "Creating units..."
 
-def create_unit(name:, serial_prefix:, manufacturer:, operator:, description:)
+# Create badge batch and badges for all units
+badge_batch = BadgeBatch.create!(note: "Seed data badges", count: 8)
+badges = 8.times.map { Badge.create!(badge_batch: badge_batch) }
+badge_ids = badges.map(&:id)
+
+def create_unit(name:, serial_prefix:, manufacturer:, operator:, description:, badge_id:)
   Unit.create!(
     SeedData.unit_fields.merge(
+      id: badge_id,
       user: $test_user,
       name: name,
       serial: "#{serial_prefix}-#{SecureRandom.hex(4).upcase}",
@@ -19,7 +25,8 @@ $castle_standard = create_unit(
   serial_prefix: "ACQ-2021",
   manufacturer: "Airquee Manufacturing Ltd",
   operator: "Stef's Castles",
-  description: "15ft x 15ft medieval themed bouncy castle with turrets"
+  description: "15ft x 15ft medieval themed bouncy castle with turrets",
+  badge_id: badge_ids[0]
 )
 
 $castle_large = create_unit(
@@ -27,7 +34,8 @@ $castle_large = create_unit(
   serial_prefix: "BCN-2020",
   manufacturer: "Bouncy Castle Boys",
   operator: "Estephan Events",
-  description: "30ft x 30ft large bouncy castle suitable for 20+ children"
+  description: "30ft x 30ft large bouncy castle suitable for 20+ children",
+  badge_id: badge_ids[1]
 )
 
 $castle_slide_combo = create_unit(
@@ -35,7 +43,8 @@ $castle_slide_combo = create_unit(
   serial_prefix: "J4J-2022",
   manufacturer: "Jump4Joy Inflatables",
   operator: "Stefan's Fun Factory",
-  description: "Pink princess themed castle with integrated 8ft slide"
+  description: "Pink princess themed castle with integrated 8ft slide",
+  badge_id: badge_ids[2]
 )
 
 $soft_play_unit = create_unit(
@@ -43,7 +52,8 @@ $soft_play_unit = create_unit(
   serial_prefix: "CIU-2023",
   manufacturer: "Custom Inflatables UK",
   operator: "Steff's Soft Play",
-  description: "Fully enclosed soft play area for under 5s"
+  description: "Fully enclosed soft play area for under 5s",
+  badge_id: badge_ids[3]
 )
 
 $obstacle_course = create_unit(
@@ -51,7 +61,8 @@ $obstacle_course = create_unit(
   serial_prefix: "IWL-2021",
   manufacturer: "Inflatable World Ltd",
   operator: "Stephan's Adventure Co",
-  description: "40ft assault course with obstacles, tunnels and slide finish"
+  description: "40ft assault course with obstacles, tunnels and slide finish",
+  badge_id: badge_ids[4]
 )
 
 $giant_slide = create_unit(
@@ -59,7 +70,8 @@ $giant_slide = create_unit(
   serial_prefix: "ACQ-2019",
   manufacturer: "Airquee Manufacturing Ltd",
   operator: "Stefan Family Inflatables",
-  description: "25ft platform height giant inflatable slide"
+  description: "25ft platform height giant inflatable slide",
+  badge_id: badge_ids[5]
 )
 
 $gladiator_duel = create_unit(
@@ -67,7 +79,8 @@ $gladiator_duel = create_unit(
   serial_prefix: "HHE-2022",
   manufacturer: "Happy Hop Europe",
   operator: "Stefano's Party Hire",
-  description: "Inflatable gladiator duel platform with pedestals"
+  description: "Inflatable gladiator duel platform with pedestals",
+  badge_id: badge_ids[6]
 )
 
 $bungee_run = create_unit(
@@ -75,7 +88,8 @@ $bungee_run = create_unit(
   serial_prefix: "PCM-2023",
   manufacturer: "Party Castle Manufacturers",
   operator: "Stef's Fun Factory",
-  description: "Two lane inflatable bungee run competition game"
+  description: "Two lane inflatable bungee run competition game",
+  badge_id: badge_ids[7]
 )
 
 Rails.logger.debug { "Created #{Unit.count} units." }
